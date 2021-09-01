c288d06e45265e5a09c1cfea938dce1b7e26db17
xScoopy <JLeary070@gmail.com>
2021-09-01 13:49:15 -0500

    Implemented validation

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -13,7 +13,14 @@ if (document.querySelector('#new-pet')) {
                 window.location.replace(`/pets/${response.data.pet._id}`);
             })
             .catch(function (error) {
-                console.log(error);
+                const alert = document.getElementById('alert')
+                alert.classList.add('alert-warning');
+                alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
+                alert.style.display = 'block';
+                setTimeout(() => {
+                    alert.style.display = 'none';
+                    alert.classList.remove('alert-warning');
+                }, 3000)
             });
     });
 }
