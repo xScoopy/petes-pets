ae4e1458df20e7e640b2bf197d785c845fe37a50
xScoopy <JLeary070@gmail.com>
2021-09-03 01:36:27 -0500

    Implemented S3 file uploads

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,16 @@
 if (document.querySelector('#new-pet')) {
     document.querySelector('#new-pet').addEventListener('submit', (e) => {
         e.preventDefault();
+        // Use FormData to grab everything now that we have files mixed in with text
+        var form = document.getElementById("new-pet");
+        var pet = new FormData(form);
 
-        let pet = {};
-        const inputs = document.querySelectorAll('.form-control');
-        for (const input of inputs) {
-            pet[input.name] = input.value;
-        }
-
-        axios.post('/pets', pet)
+        // Assign the multipart/form-data headers to axios does a proper post
+        axios.post('/pets', pet, {
+            headers: {
+                'Content-Type': 'multipart/form-data;'
+            }
+        })
             .then(function (response) {
                 window.location.replace(`/pets/${response.data.pet._id}`);
             })
--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,35 @@
 // MODELS
 const Pet = require('../models/pet');
 
+
+// UPLOADING TO AWS S3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // PET ROUTES
 module.exports = (app) => {
 
@@ -11,19 +40,31 @@ module.exports = (app) => {
     res.render('pets-new');
   });
 
-  // CREATE PET
-  app.post('/pets', (req, res) => {
-    var pet = new Pet(req.body);
+ // CREATE PET
+ app.post('/pets', upload.single('avatar'), (req, res, next) => {
+  var pet = new Pet(req.body);
+  pet.save(function (err) {
+    if (req.file) {
+      // Upload the images
+      client.upload(req.file.path, {}, function (err, versions, meta) {
+        if (err) { return res.status(400).send({ err: err }) };
 
-    pet.save()
-      .then((pet) => {
+        // Pop off the -square and -standard and just use the one URL to grab the image
+        versions.forEach(function (image) {
+          var urlArray = image.url.split('-');
+          urlArray.pop();
+          var url = urlArray.join('-');
+          pet.avatarUrl = url;
+          pet.save();
+        });
+
         res.send({ pet: pet });
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      }) ;
-  });
+      });
+    } else {
+      res.send({ pet: pet });
+    }
+  })
+})
 
   // SHOW PET
   app.get('/pets/:id', (req, res) => {
