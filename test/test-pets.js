84658f7177a8083b3513e002ea975387b1c7c655
xScoopy <JLeary070@gmail.com>
2021-09-14 19:46:17 -0500

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -127,4 +127,15 @@ it('should search ALL pets by name on /search GET', (done) => {
         done();
       });
   });
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
 });
